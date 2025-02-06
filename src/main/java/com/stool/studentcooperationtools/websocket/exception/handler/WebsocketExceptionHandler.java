d81d576a17204f76fc1ad66a01c189b7eef7cdab
laipuni <2tgb02023@gmail.com>
2025-02-06 21:30:47 +0900

    [BackEnd] refactor : 웹소켓 예외 핸들러 로그 수정

--- src/main/java/com/stool/studentcooperationtools/websocket/exception/handler/WebsocketExceptionHandler.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/exception/handler/WebsocketExceptionHandler.java
@@ -1,16 +1,16 @@
-package com.stool.studentcooperationtools.websocket.controller.exception.handler;
+package com.stool.studentcooperationtools.websocket.exception.handler;
 
+import com.stool.studentcooperationtools.exception.global.PrincipalNotFoundException;
+import com.stool.studentcooperationtools.exception.global.UnAuthorizationException;
 import com.stool.studentcooperationtools.websocket.WebsocketMessageType;
 import com.stool.studentcooperationtools.websocket.controller.Utils.SimpleMessageSendingUtils;
-import com.stool.studentcooperationtools.exception.global.PrincipalNotFoundException;
-import com.stool.studentcooperationtools.websocket.controller.exception.api.WebsocketErrorResponse;
+import com.stool.studentcooperationtools.websocket.exception.api.WebsocketErrorResponse;
 import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
@@ -25,36 +25,36 @@ public class WebsocketExceptionHandler {
 
     @MessageExceptionHandler(value = IllegalArgumentException.class)
     public void IllegalArgumentException(Message<?> message, IllegalArgumentException e) {
-        log.trace("[IllegalArgumentException] {}", e.getMessage());
+        log.trace("[{}] {}",e.getClass().getSimpleName(), e.getMessage());
         sendErrorMessage(e, getPrincipal(message));
     }
 
     @MessageExceptionHandler(value = PrincipalNotFoundException.class)
     public void IllegalArgumentException(Message<?> message, PrincipalNotFoundException e) {
-        log.warn("[PrincipalNotFoundException] {}", e.getMessage());
+        log.warn("[{}] {}",e.getClass().getSimpleName(), e.getMessage());
         sendErrorMessage(e, getPrincipal(message));
     }
 
     @MessageExceptionHandler(value = BindException.class)
     public void BindException(Message<?> message, BindException e) {
-        log.trace("[BindException] {}, {}", e.getMessage(), e.getCause());
+        log.trace("[{}] {}, {}",e.getClass().getSimpleName(), e.getMessage(), e.getCause());
         sendErrorMessage(e, getPrincipal(message));
     }
 
     @MessageExceptionHandler(value = ValidationException.class)
     public void ValidationException(Message<?> message, ValidationException e) {
-        log.trace("[ValidationException] {}, {}", e.getMessage(), e.getCause());
+        log.trace("[{}] {}, {}",e.getClass().getSimpleName(), e.getMessage(), e.getCause());
         sendErrorMessage(e, getPrincipal(message));
     }
 
-    @MessageExceptionHandler(value = AccessDeniedException.class)
-    public void AccessException(Message<?> message, AccessDeniedException e) {
-        log.trace("[AccessDeniedException] {}", e.getMessage());
+    @MessageExceptionHandler(value = UnAuthorizationException.class)
+    public void AccessException(Message<?> message, UnAuthorizationException e) {
+        log.trace("[{}] {}",e.getClass().getSimpleName(), e.getMessage());
         sendErrorMessage(e, getPrincipal(message));
     }
     @MessageExceptionHandler(value = IllegalStateException.class)
     public void BindException(Message<?> message, IllegalStateException e) {
-        log.error("[IllegalStateException] {} {}", e.getMessage(), e.getCause());
+        log.error("[{}] {} {}",e.getClass().getSimpleName(), e.getMessage(), e.getCause());
         sendErrorMessage(e, getPrincipal(message));
     }
 

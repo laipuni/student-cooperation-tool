3aec75fa2677ff5dd7e0de2f46469df232b80663
laipuni <2tgb02023@gmail.com>
2025-02-07 12:11:47 +0900

    [BackEnd] feat : api 수행 지연 로그추적기 aop 추가

--- src/main/java/com/stool/studentcooperationtools/aop/aspect/config/AspectConfig.java
+++ src/main/java/com/stool/studentcooperationtools/aop/aspect/config/AspectConfig.java
@@ -1,16 +1,13 @@
 package com.stool.studentcooperationtools.aop.aspect.config;
 
-import com.stool.studentcooperationtools.aop.LogTrace.LogTrace;
-import com.stool.studentcooperationtools.aop.aspect.LogTraceAspect;
+import com.stool.studentcooperationtools.aop.aspect.ExecutionTimeLoggerAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AspectConfig {
-
     @Bean
-    public LogTraceAspect logTraceAspect(LogTrace logTrace){
-        return new LogTraceAspect(logTrace);
+    public ExecutionTimeLoggerAspect executionTimeLoggerAspect(){
+        return new ExecutionTimeLoggerAspect();
     }
-
 }
